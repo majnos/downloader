1394af50598575177e536daaba8eb2a242a1a8c1
Jan Tomasek <jan.tomasek@iflix.com>
2017-12-05 17:39:46 +0100

    have the correct element

--- src/processor/getPages.js
+++ src/processor/getPages.js
@@ -1,31 +1,34 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
-// promise.USE_PROMISE_MANAGER = false;
+async function getMoreButtonElements(elements){
+	for (let element of elements){
+		let text = await element.getText()
+		if (text === 'VÍCE INZERÁTŮ') {
+			console.log(element.html())
+			return element
+		}
+	}		
+}
+
 async function getSourceFromUrl(url){
-	let driver = await new Builder().forBrowser('chrome').build();
-	await driver.get(url);
-	// await driver.findElement(By.name('více inzerátů')).click();
-	// await driver.findElement(By.css("div:contains('více inzerátů)")).click();
-	// let element =  await driver.findElement(By.css("#container-center > section > div.result-container > div.panel-container > div.list > div:nth-child(16) > div > button"))
-// 	driver.findElements(By.className("item")).then(function(elements){
-//     elements.forEach(function (element) {
-//         element.getText().then(function(text){
-//             console.log(text);
-//         });
-//     });
-// });
-	
-	let elements = await driver.findElements(By.className("item"))
-	for (element in elements) {
-		if (element.getText() === 'VÍCE INZERÁTŮ')
-		let rightOne = await elements[16]
-		let processed = await rightOne.getText() === 'VÍCE INZERÁTŮ'
+	try { 
+		let driver = await new Builder().forBrowser('chrome').build();
+		await driver.get(url)
+		//let elements = await driver.findElements(By.className("item"))
+		// let newElement = await getMoreButtonElements(elements)
+		// newElement.click()
+	  // await driver.findElement(By.css(("button[onclick^=addToSelected]"))).click()
+		// let element = await driver.findElement(By.xpath("//button[contains(text(),'inzer')]"))
+		// let element = await driver.findElement(By.css("button[class='ladda-button']"))
+		// let element = await driver.findElement(By.xpath("//span[text()='více inzerátů']"))
+		let element = await driver.findElement(By.className("btn btn-green"))
+		element.click().perform()
+		await driver.quit()
+		return await driver.getPageSource();
+} catch (error) {
+		console.error(error);
+		await driver.quit();		
 	}
-	console.log(processed)
-	
-	// let source = await driver.getPageSource();
-	await driver.quit();
-	return 0
 }
 
 module.exports.getSourceFromUrl = getSourceFromUrl;
