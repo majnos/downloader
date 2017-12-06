4a517f49bc0c908b6ac289634f449a3e366bc1b4
Jan Tomasek <jan.tomasek@ysoft.com>
2017-12-06 17:32:01 +0100

    next button clickable

--- src/processor/getPages.js
+++ src/processor/getPages.js
@@ -1,30 +1,19 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
-async function getMoreButtonElements(elements){
-	for (let element of elements){
-		let text = await element.getText()
-		if (text === 'VÍCE INZERÁTŮ') {
-			console.log(element.html())
-			return element
-		}
-	}		
+async function clickMore(driver){
+	let elem = await driver.findElement(By.className("btn btn-green"))
+	await driver.executeScript("arguments[0].scrollIntoView()", elem)
+	await elem.click()
 }
 
 async function getSourceFromUrl(url){
 	try { 
 		let driver = await new Builder().forBrowser('chrome').build();
 		await driver.get(url)
-		//let elements = await driver.findElements(By.className("item"))
-		// let newElement = await getMoreButtonElements(elements)
-		// newElement.click()
-	  // await driver.findElement(By.css(("button[onclick^=addToSelected]"))).click()
-		// let element = await driver.findElement(By.xpath("//button[contains(text(),'inzer')]"))
-		// let element = await driver.findElement(By.css("button[class='ladda-button']"))
-		// let element = await driver.findElement(By.xpath("//span[text()='více inzerátů']"))
-		let element = await driver.findElement(By.className("btn btn-green"))
-		element.click().perform()
-		await driver.quit()
-		return await driver.getPageSource();
+		await clickMore(driver)	
+		let source = await driver.getPageSource()
+		// await driver.quit()
+		return source
 } catch (error) {
 		console.error(error);
 		await driver.quit();		
