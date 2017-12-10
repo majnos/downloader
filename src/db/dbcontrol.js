9466d4a148d47a0df913b68095f3ff53d0c41b3e
Jan Tomasek <jan.tomasek@iflix.com>
2017-12-10 20:45:55 +0100

    db control ready

--- src/db/dbcontrol.js
+++ src/db/dbcontrol.js
@@ -1,12 +1,19 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert');
-
+const TESTDATA =  { 
+                    id: '411640',
+                    size: 'Prodej bytu 3+kk, 99 m²',
+                    href: '/nemovitosti-byty-domy/411640-nabidka-prodej-bytu-pod-sokolovnou-praha',
+                    price: '5.150.000 Kč',
+                    rooms: '3+kk' 
+                }
+const COLLECTION = 'test'
 // Connection URL
 const URL = 'mongodb://localhost:27017/myproject'
 
 async function connect(url) {
     try {
-        console.log('away' + url)
+        console.log('Connectingto server' + url)
         let db = await MongoClient.connect(url)
         console.log('Successfuly connected to the server') 
         return db
@@ -15,71 +22,64 @@ async function connect(url) {
     }
 }
 
-async function test() {
+async function insertOne(json) {
     try {
-        console.log('here:' + URL)
+        console.log('Inserting a data into the collection: '+COLLECTION+' at ' + URL)
         const db = await connect(URL)
-        
-        // await db.collection('Movies').drop()
-        await db.collection('Movies').insertMany([
-            { name: 'Enter the Dragon' },
-            { name: 'Ip Man' },
-            { name: 'Kickboxer' }
-            ]);
-    
-        // Don't `await`, instead get a cursor
-        const cursor = db.collection('Movies').find();
-        // Use `next()` and `await` to exhaust the cursor
-        for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-        console.log(doc.name);
+        await db.collection(COLLECTION).insertOne(json);
         await db.close()
-        }
     } catch (err) {
+        await db.close()        
         console.log(err)
     }
 }
 
-
-async function insert() {
-    let insertDocuments = function(db, callback) {
-        // Get the documents collection
-        let collection = db.collection('documents');
-        // Insert some documents
-        collection.insertMany([
-          {a : 1}, {a : 2}, {a : 3}
-        ], function(err, result) {
-          assert.equal(err, null);
-          assert.equal(3, result.result.n);
-          assert.equal(3, result.ops.length);
-          console.log("Inserted 3 documents into the collection");
-          callback(result);
-        });
-      }
+async function findOne(json) {
+    try {
+        const db = await connect(URL)
+        let collection = db.collection(COLLECTION)
+        let output = await collection.find(json).next()
+        await db.close()        
+        console.log(output)
+    } catch (err) {
+        await db.close()        
+        console.log(err)    
+    }
 }
-async function write2() {
-    let MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
 
-    // Connection URL
-    let url = 'mongodb://localhost:27017/myproject';
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+async function findAll(subjson)     {
+    try {
+        const db = await connect(URL)
+        let cursor = db.collection(COLLECTION).find(subjson)
+        for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+            console.log(doc);
+          }
+        await db.close()                
+    } catch (err) {
+        await db.close()        
+        console.log(err)
+    }
+}
 
-    insertDocuments(db, function() {
-    db.close();
-    });
-    });
+async function deleteOne(id) {
+    try {
+        const db = await connect(URL)
+        db.orders.deleteOne( { "_id" : ObjectId(id) } );
+        await db.close()                        
+     } catch (e) {
+        await db.close()                        
+        print(e);
+     }
 }
 
 async function close() {
     db.close()
 }
 
-
 (async () => {
     console.log('start pico vole pico')
-    let data = await test()
-    }
-  )()
+    await insertOne({text: 'ahojhele'})
+    await findAll(
+        {}
+    )
+    })()
